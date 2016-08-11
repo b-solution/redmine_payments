b9b49b3eecd5c48ac40e6ba02db1e38dd5482b82
bilel-kedidi <bilel.kedidi@gmail.com>
2016-08-11 20:04:01 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get 'create_order', controller: 'payments', action: 'create_order'
-get 'add_item', controller: 'payments', action: 'add_item'
-get 'charge_order', controller: 'payments', action: 'charge_order'
-get 'add_lead', controller: 'payments', action: 'add_lead'
-
-get 'get_products', controller: 'products', action: 'get_products'
 
 post 'create_order', controller: 'payments', action: 'create_order', via: :options
 post'add_item', controller: 'payments', action: 'add_item' , via: :options
@@ -14,6 +8,14 @@ post 'charge_order', controller: 'payments', action: 'charge_order', via: :optio
 post 'add_lead', controller: 'payments', action: 'add_lead', via: :options
 
 post 'get_products', controller: 'products', action: 'get_products', via: :options
+
+get 'create_order', controller: 'payments', action: 'create_order'
+get 'add_item', controller: 'payments', action: 'add_item'
+get 'charge_order', controller: 'payments', action: 'charge_order'
+get 'add_lead', controller: 'payments', action: 'add_lead'
+
+get 'get_products', controller: 'products', action: 'get_products'
+
 
 resources :projects do
   resources :products do
